f675a1a8981b1adb0215b544cc3cd4672dfbb2b4
tippettj <tippettj@gmail.com>
2019-11-19 11:13:35 -0800

    Updates

---
+++ precache-manifest.55ecc12f6d647488136212239b7db08c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46a27d6c7c7856ec1bd312dc7deeaab1",
+    "url": "/react-tut/index.html"
+  },
+  {
+    "revision": "e5b3893bec3bd8d3fea4",
+    "url": "/react-tut/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "f44ec3f62f7c05e9ffd9",
+    "url": "/react-tut/static/js/2.8ca14d58.chunk.js"
+  },
+  {
+    "revision": "e5b3893bec3bd8d3fea4",
+    "url": "/react-tut/static/js/main.845aaa5d.chunk.js"
+  },
+  {
+    "revision": "5bc54786cad42af3849f",
+    "url": "/react-tut/static/js/runtime-main.8b38b508.js"
+  }
+]);
